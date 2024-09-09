3c5405488c87c8a91fb78a45463aec42bfb5eb94
ahmed najash <ahmednjsh1@gmail.com>
2024-09-09 21:32:16 +0300

    Add challenge 4 solution

---
+++ Assets/Scripts/SpawnManagerX.cs
@@ -0,0 +1,82 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnManagerX : MonoBehaviour
+{
+    public GameObject enemyPrefab;
+    public GameObject powerupPrefab;
+
+    private float spawnRangeX = 10;
+    private float spawnZMin = 15; // set min spawn Z
+    private float spawnZMax = 25; // set max spawn Z
+
+    public int enemyCount;
+    public int waveCount = 1;
+
+    public GameObject player;
+
+    // Speed to increase per wave
+    private float enemySpeed = 10.0f;
+
+    private void Start()
+    {
+        SpawnEnemyWave(waveCount);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // Count the number of enemies in the scene
+        enemyCount = GameObject.FindGameObjectsWithTag("Enemy").Length;
+
+        // If no enemies are left, spawn a new wave
+        if (enemyCount == 0)
+        {
+            waveCount++;
+            SpawnEnemyWave(waveCount);
+        }
+    }
+
+    // Generate random spawn position for powerups and enemy balls
+    Vector3 GenerateSpawnPosition()
+    {
+        float xPos = Random.Range(-spawnRangeX, spawnRangeX);
+        float zPos = Random.Range(spawnZMin, spawnZMax);
+        return new Vector3(xPos, 0, zPos);
+    }
+
+    void SpawnEnemyWave(int enemiesToSpawn)
+    {
+        Vector3 powerupSpawnOffset = new Vector3(0, 0, -15); // make powerups spawn at player end
+
+        // If no powerups remain, spawn a powerup
+        if (GameObject.FindGameObjectsWithTag("Powerup").Length == 0) // check that there are zero powerups
+        {
+            Instantiate(powerupPrefab, GenerateSpawnPosition() + powerupSpawnOffset, powerupPrefab.transform.rotation);
+        }
+
+        // Spawn number of enemies based on wave number
+        for (int i = 0; i < enemiesToSpawn; i++)
+        {
+            GameObject newEnemy = Instantiate(enemyPrefab, GenerateSpawnPosition(), enemyPrefab.transform.rotation);
+            EnemyX enemyScript = newEnemy.GetComponent<EnemyX>();
+            if (enemyScript != null)
+            {
+                // Increase the speed of the enemies based on the wave count
+                enemyScript.speed += (enemySpeed * waveCount);
+            }
+        }
+
+        // Reset player position after spawning enemies
+        ResetPlayerPosition();
+    }
+
+    // Move player back to position in front of own goal
+    void ResetPlayerPosition()
+    {
+        player.transform.position = new Vector3(0, 1, -7);
+        player.GetComponent<Rigidbody>().velocity = Vector3.zero;
+        player.GetComponent<Rigidbody>().angularVelocity = Vector3.zero;
+    }
+}
